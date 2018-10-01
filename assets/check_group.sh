6b40bfb6fc8ce81f9c0608fe1dae48e64db437d7
phossner <phossner@ukaachen.de>
2018-10-01 12:53:54 +0200

    Log to /dev/stdout instead of /dev/console

--- assets/check_group.sh
+++ assets/check_group.sh
@@ -4,6 +4,6 @@ USER=${PAM_USER}
 GROUP=$(echo $REQUEST | awk '{print $2;}' | tr -d '/')
 
 # check if user is in group
-echo "PAM: Checking if user ${USER} is in group ${GROUP}..." > /dev/console
+echo "PAM: Checking if user ${USER} is in group ${GROUP}..." > /dev/stdout
 id -Gn ${USER} | tr " " "\n" | grep -xq ${GROUP}
 exit $?
