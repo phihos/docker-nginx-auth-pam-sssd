794956dd75890aae5d1cf59bd6773df2c2683309
Philipp Hossner <philipp.hossner@posteo.de>
2018-11-08 13:16:57 +0100

    Urldecode group parameter in check_group.sh

--- assets/check_group.sh
+++ assets/check_group.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
+
 USER=${PAM_USER}
 GROUP=$(echo $REQUEST | awk '{print $2;}' | tr -d '/')
+GROUP=$(urldecode "${GROUP}")
 
 # check if user is in group
 echo "PAM: Checking if user ${USER} is in group ${GROUP}..." > /dev/stdout
