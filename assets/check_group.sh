a816cfa5dc7f160c35c77251aae918a3bdfa637a
Philipp Hossner <philipp.hossner@posteo.de>
2018-11-08 12:51:27 +0100

    Fix groups with special characters causing the check_group.sh script to crash

--- assets/check_group.sh
+++ assets/check_group.sh
@@ -5,5 +5,5 @@ GROUP=$(echo $REQUEST | awk '{print $2;}' | tr -d '/')
 
 # check if user is in group
 echo "PAM: Checking if user ${USER} is in group ${GROUP}..." > /dev/stdout
-id -Gn ${USER} | tr " " "\n" | grep -xq ${GROUP}
+id -Gn ${USER} | tr " " "\n" | grep -xq "${GROUP}"
 exit $?
